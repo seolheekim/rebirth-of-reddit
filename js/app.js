243fcc9b07c59f76242d720eef59a14a56b1a5fd
Seolhee Kim <seolhee@upspringmedia.com>
2017-06-13 15:08:57 -1000

    working on image

--- js/app.js
+++ js/app.js
@@ -1,57 +1,75 @@
+
 (function (){
-
-  var mainContainer = document.getElementById("Container");
-
-  var postContainer = document.createElement("div");
-  postContainer.id = "PostContainer";
-  mainContainer.appendChild(postContainer);
 
   function reqRedditListener(){
     var res = JSON.parse(this.responseText);
     // console.log("HI", res)
     // console.log(res.data.children)
     // console.log(res.data.title)
+    var mainContainer = document.getElementById("Container");
+
+    var postContainer = document.createElement("div");
+    postContainer.id = "PostContainer";
+    mainContainer.appendChild(postContainer);
 
     var currentPost;
 
     for(var i = 0; i < res.data.children.length; i++){
+      for(var x = 0; x < 4; x++){
+        currentPost = res.data.children[x].data;
+        if(currentPost.stickied === false) {
 
-    currentPost = res.data.children[i].data
+        var newDiv = document.createElement("div");
+        newDiv.id = "box"
+        postContainer.appendChild(newDiv)
 
-    var newDiv = document.createElement("div");
-    newDiv.id = "box"
-    postContainer.appendChild(newDiv)
+        for(var s = 0; s < currentPost.preview.images[0]; s++) {
 
-    var images = document.createElement("img")
-    images.src = currentPost.thumbnail
-    images.setAttribute('href', currentPost.url)
-    images.className = "Images";
-    newDiv.appendChild(images);
+        var newImgDiv = document.createElement("div");
+        newImgDiv.className = "imgDiv";
+        newDiv.appendChild(newImgDiv);
 
-    var titles = document.createElement("a");
-    titles.className = "Titles";
-    titles.setAttribute('href', currentPost.url)
-    titles.innerHTML = currentPost.title
-    // console.log(res.data.children[i].title)
-    console.log(res.data.children[i])
-    newDiv.appendChild(titles);
+        var img = document.createElement("img");
+        img.src = currentPost.images[0].source.url
+        img.className = "Images";
+        img.setAttribute('href', currentPost.url);
+        newImgDiv.appendChild(img);
 
-    var author = document.createElement("p");
-    author.className = "Author";
-    author.setAttribute('href', currentPost.user)
-    author.innerHTML = currentPost.author
-    newDiv.appendChild(author);
+         function imageError(images){
+          var replaceImages = document.getElementsByClassName("Images");
+          if(replaceImages === null){
+            replaceImages.src = "/assets/image_placeholder.png";
+          }
+         }//ending imageError function
+        }//ending image forloop
 
-    var commentsCount = document.createElement("p");
-    commentsCount.className = "CommentsCount";
-    commentsCount.innerHTML = currentPost.num_comments;
-    newDiv.appendChild(commentsCount);
+        var titles = document.createElement("a");
+        titles.className = "Titles";
+        titles.setAttribute('href', currentPost.url);
+        titles.innerHTML = currentPost.title;
+        // console.log(res.data.children[i].title)
+        // console.log(res.data.children[x])
+        newDiv.appendChild(titles);
 
+        var author = document.createElement("a");
+        author.className = "Author";
+        author.setAttribute('href', currentPost.url);
+        author.innerHTML = "by " + currentPost.author;
+        newDiv.appendChild(author);
 
+        var commentsCount = document.createElement("a");
+        commentsCount.className = "CommentsCount";
+        commentsCount.innerHTML = "comments ".link("http://www.reddit.com" + currentPost.permalink) + currentPost.num_comments;
+        newDiv.appendChild(commentsCount);
 
+        var timeStamp = document.createElement("p");
+        timeStamp.className = "timeStamp";
+        timeStamp.innerHTML = new Date(currentPost.created * 1000)
+        newDiv.appendChild(timeStamp);
 
-
-   }//ending my forloop for res data
+      }//endings post count forloop
+    }// ending stickied if statment
+  }//ending my forloop for res data
 }// ending my reqRedditListener function
 
 var rReq = new XMLHttpRequest();
